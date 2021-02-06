085ebb27a5ef6411f282f69290a3bca08f161322
elias <pakatsy@ciencias.unam.mx>
2021-02-06 16:57:40 -0600

    primera solucion inmediata

---
+++ calculateValue.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * Obtiene el valor que corresponde dado un numero
+ * @param {Number} n 
+ * @returns {Number | String} valor que corresponde
+ * Multiplo de 3 => 'Music'
+ * Multiplo de 5 => 'TI'
+ * Multiplo de 3 y 5 => 'Musical'
+ * En otro caso regresa el mismo numero
+ */
+function calculateValue(n) {
+    let value = n
+
+    !(n % 3) && (value = 'Music')
+    !(n % 5) && (value = 'TI')
+    !(n % 15) && (value = 'Musical') // minimo comun multiplo:(3,5) = 15
+
+    return value
+}
+
+
+module.exports = { calculateValue }
---
+++ challenge.js
@@ -0,0 +1,13 @@
+/**
+ * CHALLENGE BACKEND.
+ */
+
+const { calculateValue } = require("./calculateValue");
+
+
+for (let i = 0; i <= 100; i++) {
+    console.log(calculateValue(i))
+}
+
+
+
